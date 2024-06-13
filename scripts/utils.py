f0b473c6e5b2013b7992dc6efb47c76374f8d0f3
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-06-13 14:14:07 -0400

    removed the unnecessary page id

--- scripts/utils.py
+++ scripts/utils.py
@@ -8,6 +8,8 @@ PARENTDIR = os.path.dirname(os.getcwd()) #os.path.dirname(os.path.abspath(__file
 
 ## the api doesn't allow access to this dataset directly
 ## so we have to use page numbers to scrape the data
+## the api doesn't allow access to this dataset directly
+## so we have to use page numbers to scrape the data
 def get_data(url, table_name="table"):
     i = 1
     data = {}
@@ -19,13 +21,14 @@ def get_data(url, table_name="table"):
         # print("On page: ", i)
         # print(url.replace("=pagenum", f"={i}"))
         for tb in BeautifulSoup(response.content, 'html.parser').find_all("div", {"class":"DataContainer"}):
-            data[i] = {x.text:[] for x in tb.find_all('th') if x is not None}
+            if i == 1:
+                data = {x.text:[] for x in tb.find_all('th') if x is not None}
             for row in tb.find_all('tr'):
                 if row.find('td') is not None:
                     vals = [v.text for v in row.find_all('td') if v is not None]
-                    for pos, k in enumerate(data[i]):
+                    for pos, k in enumerate(data):
                         if len(vals) > pos:
-                            data[i][k].append(vals[pos])
+                            data[k].append(vals[pos])
         i += 1
         response = requests.get(url.replace("=pagenum", f"={i}"))
     return data
