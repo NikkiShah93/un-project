a88639f78fbd50a9de9d5718b9009c616dfab56f
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-06-13 14:07:33 -0400

    fixed the issue with mongodb manager

--- scripts/mongo_manager.py
+++ scripts/mongo_manager.py
@@ -6,15 +6,21 @@ client = MongoClient("localhost", 27017)
 def create_meta(PATH = '\\data\\table-list.json',
                  dbname = 'test_db', 
                  parent=True,
+                 meta_name = "mdata",
                  client=client):
     table_list = get_file(PATH, parent=parent)
     ## create a database
     table_db = client[dbname]
-    if "mdata" in table_db.list_collection_names():
-        table_db['mdata'].drop()
+    if meta_name in table_db.list_collection_names():
+        table_db[meta_name].drop()
     ## creating the metadata table
     mdata = table_db.mdata
     for table, val in table_list.items():
         mdata.insert_one({"name":table, "values":val}) 
-    return mdata.count_documents()
+    return mdata.count_documents({})
+
+def get_meta(table_name = "mdata",
+             db_name="test_db",
+             client=client):
+    pass
 
